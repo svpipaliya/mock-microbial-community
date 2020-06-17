10ad7eeeb3f5e094092ee5fc732ec1a0b2ecdd62
svpipaliya <61172011+svpipaliya@users.noreply.github.com>
2020-06-17 11:56:33 -0600

    Update and rename kraken_example_w (1).sh to kraken_example_w.sh

--- kraken_example_w.sh
+++ kraken_example_w.sh
@@ -10,7 +10,7 @@
 
 module load gcc/7.3.0
 module load nixpkgs/16.09
-module load kraken2/2.0.7-beta
+module load kraken2/2.0.8-beta
 
 kraken2 -db kraken2-microbial-fatfree --threads 16 --report SRR3178018.fastq.report --unclassified-out SRR3178018.fastqunclassified#.fq --classified-out SRR3178018.fastq.fastqclassified#.fq --paired SRR3178018_1.fastq SRR3178018_2.fastq > SRR3178018.fastq.Kraken.out
 #end
